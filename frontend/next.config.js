78dcae545b45ffa4dea001a555be39de576895f4
vijay-fs <vijaykarthik9868@gmail.com>
2023-10-27 21:25:57 +0530

    latest changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,18 +7,18 @@ const nextConfig = {
       appDir: true,
     },
     images: {
-      remotePatterns: [
-        // {
-        //   protocol: 'http',
-        //   hostname: '127.0.0.1',
-        //   port: '1337',
-        //   pathname: '/uploads/**',
-        // },
-        {
-          protocol: 'https',
-          hostname: 'images.pexels.com',
-        }
-      ],
+      // remotePatterns: [
+      //   {
+      //     protocol: 'http',
+      //     hostname: '127.0.0.1',
+      //     port: '1337',
+      //     pathname: '/uploads/**',
+      //   },
+      //   {
+      //     protocol: 'https',
+      //     hostname: 'images.pexels.com',
+      //   }
+      // ],
     },
   }
   
